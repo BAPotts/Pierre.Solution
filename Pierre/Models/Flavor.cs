f25e47d3473c8c7bddb38190052bbdfb880d9b6f
Beverly Potts <pottsbeverly@gmail.com>
2020-08-14 13:02:26 -0400

    Fix typo: String -> string

--- Pierre/Models/Flavor.cs
+++ Pierre/Models/Flavor.cs
@@ -10,7 +10,7 @@ namespace Pierre.Models
     }
 
     public int  FlavorId { get; set; }
-    public String Name { get; set; }
+    public string Name { get; set; }
     public virtual ICollection<TreatFlavor> Treats { get; set; }
   }
 }
